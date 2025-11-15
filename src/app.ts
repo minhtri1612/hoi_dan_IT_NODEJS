0bb0b726ca654aa20e8440dff4768f8f01581223
minhtri1612 <10422119@student.vgu.edu.vn>
2025-11-15 11:29:49 +0700

    setup devtool and env

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World and vc');
 });
 
 app.get('/hoidanit', (req, res) => {
     res.send('Hello Minh Tri!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port:`,process.env.PORT);
 });
